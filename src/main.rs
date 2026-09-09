dea1042938560a2749aadbf4dd9370eaa3eaef95
natokazen <natokazen@gmail.com>
2026-09-09 10:09:05 +0300

    Basic conv from fah to cels, Done! ✅

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 fn main() {
     println!(r#"
         ░▀█▀░█▀▀░█▄█░█▀█░░░█▀▀░█▀█░█▀█░█░█░░░█▀▀░█░░░▀█▀
@@ -5,5 +7,17 @@ fn main() {
         ░░▀░░▀▀▀░▀░▀░▀░░░░░▀▀▀░▀▀▀░▀░▀░░▀░░░░▀▀▀░▀▀▀░▀▀▀
 
     "#);
-    println!("   [ Options: Fahrenheit to Celsius [1] | Celsius to Fahrenheit [2] ]");
+    // println!("   [ Options: Fah to Cels [1] | Cels to Fah [2] | Quit [q] ]");
+
+        let mut number = String::new();
+
+        print!("Enter a number: ");
+        io::stdout().flush().expect("Flush failed");
+
+        io::stdin().read_line( &mut number).expect("Failed to read input!");
+        let number :f64 = number.trim().parse().expect("Failed to parse input");
+
+        let calc_fah_to_cels = (number - 32.0 ) * 5.0 / 9.0;
+        println!("Number converted is : {}", calc_fah_to_cels.floor());
+
 }
