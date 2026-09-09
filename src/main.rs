645aea5dfd63408e1c2a0e55cc8342ac249149b5
natokazen <natokazen@gmail.com>
2026-09-09 10:43:20 +0300

    Now you can do both side convertion

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,64 @@
 use std::io::{self, Write};
 
 fn main() {
-    println!(r#"
+    println!(
+        r#"
         ░▀█▀░█▀▀░█▄█░█▀█░░░█▀▀░█▀█░█▀█░█░█░░░█▀▀░█░░░▀█▀
         ░░█░░█▀▀░█░█░█▀▀░░░█░░░█░█░█░█░▀▄▀░░░█░░░█░░░░█░
         ░░▀░░▀▀▀░▀░▀░▀░░░░░▀▀▀░▀▀▀░▀░▀░░▀░░░░▀▀▀░▀▀▀░▀▀▀
 
-    "#);
-    // println!("   [ Options: Fah to Cels [1] | Cels to Fah [2] | Quit [q] ]");
+    "#
+    );
+    println!("   [ Options: Fah to Cels [1] | Cels to Fah [2] | Quit [q] ]");
 
-        let mut number = String::new();
+    loop {
 
-        print!("Enter a number: ");
+        let mut option_input = String::new();
+
+        print!("\n Enter an option: ");
         io::stdout().flush().expect("Flush failed");
 
-        io::stdin().read_line( &mut number).expect("Failed to read input!");
-        let number :f64 = number.trim().parse().expect("Failed to parse input");
+        io::stdin().read_line(&mut option_input).expect("Failed to read input!");
+        let option_input = option_input.trim();
 
-        let calc_fah_to_cels = (number - 32.0 ) * 5.0 / 9.0;
-        println!("Number converted is : {}", calc_fah_to_cels.floor());
+        if option_input == "1" {
+            let mut number = String::new();
 
+            print!("\n Chose option [1] | Enter a number: ");
+            io::stdout().flush().expect("Flush failed");
+
+            io::stdin()
+                .read_line(&mut number)
+                .expect("Failed to read input!");
+            let number: f64 = number.trim().parse().expect("Failed to parse input");
+
+            let calc_cels = calc_fah_to_cels(number);
+            println!(" Number converted Fahrenheit to Celsius is : {}°C", calc_cels.round());
+            break;
+
+        } else if option_input == "2" {
+            let mut number = String::new();
+
+            print!("\n Chose option [2] | Enter a number: ");
+            io::stdout().flush().expect("Flush failed");
+
+            io::stdin()
+                .read_line(&mut number)
+                .expect("Failed to read input!");
+            let number: f64 = number.trim().parse().expect("Failed to parse input");
+
+            let calc_fah = calc_cels_to_fah(number);
+
+            println!(" Number converted from Celsius to Fahrenheit is : {}°F", calc_fah.round());
+            break;
+        }
+    }
+}
+
+fn calc_fah_to_cels(num: f64) -> f64 {
+    (num - 32.0) * 5.0 / 9.0
+}
+
+fn calc_cels_to_fah(num: f64) -> f64 {
+    (num + 32.0) * 9.0 / 5.0
 }
